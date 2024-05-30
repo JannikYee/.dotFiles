dd5d0cd83bc15c0ccd5fab6ba6a201230f55c92f
Jannik Ihler <jannikihler@web.de>
2024-05-30 20:58:24 +0200

    Complete spliting the config

--- flake.nix
+++ flake.nix
@@ -7,15 +7,33 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./Desktop/configuration.nix
-      ];
+  outputs = {
+    self,
+    nixpkgs
+  }: let 
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in{
+    nixosConfigurations = {
+      nixos-desktop = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./desktop/base.nix
+        ];
+      };
     };
   };
+
+  #outputs = { self, nixpkgs, ... }@inputs: {
+  #  # Please replace my-nixos with your hostname
+  #  nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+  #    system = "x86_64-linux";
+  #    modules = [
+  #      # Import the previous configuration.nix we used,
+  #      # so the old configuration file still takes effect
+  #      ./Desktop/configuration.nix
+  #    ];
+  #  };
+  #};
 }
