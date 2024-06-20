ea3915bf2c559d8bfd8d6e16a2fc22eaee9ed604
Jannik <jannikihler@web.de>
2024-06-20 11:04:00 +0200

    Barrier Freigabe hinzugefügt

--- desktop-hp/base.nix
+++ desktop-hp/base.nix
@@ -15,6 +15,8 @@
   networking.hostName = "nixos-desktop-hp"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  networking.firewall.allowedTCPPorts = [24800]; #Barrier freigabe
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
